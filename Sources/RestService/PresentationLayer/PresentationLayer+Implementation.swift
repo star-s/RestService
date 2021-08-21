b987c0ce265800830c136fef8ce132ee29773318
Sergey Starukhin <star.s@me.com>
2021-08-21 21:37:16 +0200

    code has been formatted

--- Sources/RestService/PresentationLayer/PresentationLayer+Implementation.swift
+++ Sources/RestService/PresentationLayer/PresentationLayer+Implementation.swift
@@ -12,27 +12,41 @@ public extension PresentationLayer {
     var defaultHeaders: HTTPHeaders { .default }
     
     func prepare<T: Encodable>(post url: URL, parameters: T) throws -> URLRequest {
-        try URLRequest(url: url).settingHeaders(defaultHeaders).settingMethod(.post).encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
+        try URLRequest(url: url)
+            .settingHeaders(defaultHeaders)
+            .settingMethod(.post)
+            .encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
     }
     
     func prepare<T: Encodable>(get url: URL, parameters: T) throws -> URLRequest {
-        try URLRequest(url: url).settingHeaders(defaultHeaders).encodingQuery(parameters, coder: URLEncodedFormEncoder())
+        try URLRequest(url: url)
+            .settingHeaders(defaultHeaders)
+            .encodingQuery(parameters, coder: URLEncodedFormEncoder())
     }
     
     func prepare<T: Encodable>(put url: URL, parameters: T) throws -> URLRequest {
-        try URLRequest(url: url).settingHeaders(defaultHeaders).settingMethod(.put).encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
+        try URLRequest(url: url)
+            .settingHeaders(defaultHeaders)
+            .settingMethod(.put)
+            .encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
     }
     
     func prepare<T: Encodable>(patch url: URL, parameters: T) throws -> URLRequest {
-        try URLRequest(url: url).settingHeaders(defaultHeaders).settingMethod(.patch).encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
+        try URLRequest(url: url)
+            .settingHeaders(defaultHeaders)
+            .settingMethod(.patch)
+            .encodingBody(parameters, coder: JSONEncoder(), contentType: "application/json")
     }
     
     func prepare<T: Encodable>(delete url: URL, parameters: T) throws -> URLRequest {
-        try URLRequest(url: url).settingHeaders(defaultHeaders).settingMethod(.delete).encodingQuery(parameters, coder: URLEncodedFormEncoder())
+        try URLRequest(url: url)
+            .settingHeaders(defaultHeaders)
+            .settingMethod(.delete)
+            .encodingQuery(parameters, coder: URLEncodedFormEncoder())
     }
     
     func validate(data: Data, response: URLResponse) throws -> Data {
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
             throw URLError(.badServerResponse)
         }
         return data
